f49160e151bbed5b8411cc732f4f2ccc490e23f3
larcade-tess <35175520+larcade-tess@users.noreply.github.com>
2018-02-09 19:40:54 -0700

    Added week 3 and 4

---
+++ items.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['items'])) {
+	$_SESSION['items'] = array();
+}
+
+if (isset($_POST['item'])) {
+	array_push($_SESSION['items'], $_POST['item']);
+}
+?>
--- week3.js
+++ week3.js
@@ -1,15 +1,61 @@
- function addItem(id) {
+function string() {
+	var snacks = ["Chips", "Goldfish", "Crackers", "Pretzels", "Apples"];
+	var jsonStringArray = JSON.stringify(snacks);
+	document.getElementById("stringArr").innerHTML = jsonStringArray;
+
+	var info = {"Phone":"8014323442", "Email":"something@gmail.com", "Date":new Date()};
+	var jsonStringObj = JSON.stringify(info);
+	document.getElementById("stringObj").innerHTML = jsonStringObj;
+}
+
+function parse() {
+	var snacks = ["Chips", "Goldfish", "Crackers", "Pretzels", "Apples"];
+	var jsonStringArray = JSON.stringify(snacks);
+	var jsonParseArray = JSON.parse(jsonStringArray);
+	document.getElementById("parseArr").innerHTML = jsonParseArray;
+
+	var info = {"Phone":"8014323442", "Email":"something@gmail.com", "Date":new Date()};
+	var jsonStringObj = JSON.stringify(info);
+	var jsonParseObj = JSON.parse(jsonStringObj);
+	document.getElementById("parseObj").innerHTML = jsonParseObj.Phone + ", " + jsonParseObj.Email + ", " + jsonParseObj.Date;
+}
+
+function pokemon() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4) {
 			if (this.status == 200) {
-				console.log('ajax response', this.responseText);
+				var jobj = JSON.parse(this.responseText);
+				document.getElementById("pokemon").innerHTML = "Pokemon ID: " + jobj.id + ", Pokemon Name: " + jobj.name + ", Base Experience: " + jobj.base_experience;
 			} else {
 				console.log('not cool');
 			}
 		}
 	};
-	xhttp.open("POST", "https://calm-wave-20284.herokuapp.com/cart.php", true);
-	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xhttp.send('item=' + id);
+	xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/1/", true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+	xhttp.send();
+
+}
+
+function math() {
+	var mathChoice = document.querySelector('select');
+	var problemDisplay = document.querySelector('pre');
+
+	function onchange() {
+		var math = mathChoice.value;
+		updateDisplay(math);
+	};
+	onchange();
+
+	function updateDisplay(math) {
+		math = math.replace(" ", "");
+		math = math.toLowerCase();
+		var url = math + '.txt';
+		fetch(url).then(function(response) {
+			response.text().then(function(text) {
+				problemDisplay.textContent = text;
+			});
+		});
+	};
 }
